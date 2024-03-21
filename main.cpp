556fa06ea36d37699ed23d134935eb4f7cec5963
Mario Simental <simentalma@yahoo.com>
2024-03-21 11:35:38 -0500

    Finished

--- main.cpp
+++ main.cpp
@@ -1,36 +1,112 @@
 #include <iostream>
 #include <fstream>
 
-int** initMatrix(int size) {
+// Initializes a square matrix with given size with zeros.
+int** initMatrix(int size){
     int** matrix = new int*[size];
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++){
         matrix[i] = new int[size];
-        for (int j = 0; j < size; j++) {
+        for (int j = 0; j < size; j++){
             matrix[i][j] = 0;
         }
     }
     return matrix;
 }
 
-void freeMatrix(int** matrix, int size) {
-    for (int i = 0; i < size; i++) {
+void freeMatrix(int** matrix, int size){
+    for (int i = 0; i < size; i++){
         delete[] matrix[i];
     }
     delete[] matrix;
 }
 
-void printMatrix(int** matrix, int size) {
+void printMatrix(int** matrix, int size){
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            std::cout << matrix[i][j] << " ";
+            std::cout << matrix[i][j] << "\t";
         }
         std::cout << "\n";
     }
 }
 
+void populateMatrix(std::ifstream& infile, int** matrix, int size){
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if (!(infile >> matrix[i][j])){
+                std::cerr << "Error reading from file.";
+                freeMatrix(matrix, size);
+                infile.close();
+                exit(1);
+            }
+        }
+    }
+}
+
+int** matrixSum(int** matrix1, int** matrix2, int size){
+    int** matrix = initMatrix(size);
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    return matrix;
+}
+
+int** matrixProduct(int** matrix1, int** matrix2, int size){
+    int** matrix = initMatrix(size);
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            for(int k = 0; k < size; k++){
+                matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    return matrix;
+}
+
+int** matrixDifference(int** matrix1, int** matrix2, int size){
+    int** matrix = initMatrix(size);
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            matrix[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+    return matrix;
+}
+
+int getMaxElement(int** matrix, int size) {
+    int maxElement = matrix[0][0]; 
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (matrix[i][j] > maxElement) { 
+                maxElement = matrix[i][j];
+            }
+        }
+    }
+    return maxElement;
+}
+
+void updateElement(int** matrix, int size, int row, int col, int value) {
+    if (row >= size || col >= size) {
+        std::cerr << "Out of bounds.";
+        freeMatrix(matrix, size);
+        exit(1);
+    }
+    matrix[row][col] = value;
+}
+
+void transposeMatrix(int** matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
 
 int main() {
-    std::ifstream infile("input.txt");
+    std::ifstream infile("matrix_input.txt");
 
     if (!infile) {
         std::cerr << "Error: Unable to open the input file.";
@@ -40,11 +116,46 @@ int main() {
     int size;
     infile >> size;
 
-    int** matrix = initMatrix(size);
+    int** matrix1 = initMatrix(size);
+    int** matrix2 = initMatrix(size);
 
-    printMatrix(matrix, size);
+    populateMatrix(infile, matrix1, size);
+    populateMatrix(infile, matrix2, size);
 
-    freeMatrix(matrix, size);
+    std::cout << "Matrix 1:\n";
+    printMatrix(matrix1, size);
+    std::cout << "\nMatrix 2:\n";
+    printMatrix(matrix2, size);
+
+    std::cout << "\nSum of Matrix 1 and Matrix 2:\n";
+    int** matrix3 = matrixSum(matrix1, matrix2, size);
+    printMatrix(matrix3, size);
+
+    std::cout << "\nProduct of Matrix 1 and Matrix 2:\n";
+    int** matrix4 = matrixProduct(matrix1, matrix2, size);
+    printMatrix(matrix4, size);
+
+    std::cout << "\nDifference of Matrix 1 and Matrix 2:\n";
+    int** matrix5 = matrixDifference(matrix1, matrix2, size);
+    printMatrix(matrix5, size);
+
+    std::cout << "\nChange (1,1) of Matrix 1 to 100:\n";
+    updateElement(matrix1, size, 1, 1, 100);
+    printMatrix(matrix1, size);
+
+    std::cout << "\nMax Element of Matrix 1:\n";
+    int maxElement = getMaxElement(matrix1, size); 
+    std::cout << maxElement << std::endl; 
+
+    std::cout << "\nTransposition of Matrix 1:\n";
+    transposeMatrix(matrix1, size);
+    printMatrix(matrix1, size);
+
+    freeMatrix(matrix1, size);
+    freeMatrix(matrix2, size);
+    freeMatrix(matrix3, size);
+    freeMatrix(matrix4, size);
+    freeMatrix(matrix5, size);
 
     infile.close();
     return 0;
